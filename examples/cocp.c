224bc86f67b1fdf05a2f7439b470b19eb8db4921
Your Name <your.email@example.com>
2026-07-01 15:46:46 +0800

    examples of constant folding/propagation

---
+++ examples/cocp.c
@@ -0,0 +1,6 @@
+/*constant propagation example*/
+
+int main() {
+    int x = 50 + 50 / 7;
+    return x;
+}
---
+++ examples/coptf.c
@@ -0,0 +1,7 @@
+/*example constant folding*/
+
+int main() {
+    int x;
+    x = 50 + 50 / 7;
+    return x;
+}
